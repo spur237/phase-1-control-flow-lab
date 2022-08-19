47f6761a09243610ffad8fdb5d526ccec7019c3b
spur237 <samuel.nzioka@student.moringaschool.com>
2022-08-19 17:26:21 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
 
-function ternaryCheckCity(){
-  // Write your code here!
+function scuberGreetingForFeet(stage){
+  let cost;
+  if (stage <= 400 ) {
+    cost = 'This one is on me!';
+  }
+  else if (stage > 2000 && stage<2500) {
+    cost = 'I will gladly take your thirty bucks.';
+  }
+  else if(stage > 2500){
+    cost = 'No can do.'
+  }
+return cost;
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let result  = city==="NYC" ? "Ok, sounds good." : "No go.";
+  return result;
+}
+function switchOnCharmFromTip(tip){
+  let result;
+  switch(tip) {
+    case 'generous':
+      result = "Thank you so much.";
+      break;
+    case 'not as generous':
+     result = "Thank you.";
+      break;
+    default:
+      result = "Bye.";
+      break;
+  }
+  return result;
 }
